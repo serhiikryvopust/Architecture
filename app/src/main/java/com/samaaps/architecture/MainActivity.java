1237965b7929b13c9dc297be366d8898875d8412
Sam <serhii@kryvopust@globallogic.com>
2020-12-07 19:48:04 +0200

    add new activity

--- app/src/main/java/com/samaaps/architecture/MainActivity.java
+++ app/src/main/java/com/samaaps/architecture/MainActivity.java
@@ -2,13 +2,25 @@ package com.samaaps.architecture;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button btnStart;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnStart = findViewById(R.id.btnStart);
+
+        btnStart.setOnClickListener(view -> {
+            Intent intent = new Intent(this,SecondActivity.class);
+            startActivity(intent);
+        });
+
     }
 }
