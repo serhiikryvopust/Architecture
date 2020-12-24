ab3dbe18a4149ef9f0b470b8cd41b88624bf0612
Sam <serhii@kryvopust@globallogic.com>
2020-12-24 10:07:26 +0200

    add call interface

--- app/src/main/java/com/samaaps/architecture/MainActivity.java
+++ app/src/main/java/com/samaaps/architecture/MainActivity.java
@@ -8,10 +8,16 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
 public class MainActivity extends AppCompatActivity {
     Button btnLogin;
     EditText etLogin;
     EditText etPassword;
+    ArrayList<UserModel> users;
+    HashMap<Integer,UserModel> userTypes;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,8 +28,10 @@ public class MainActivity extends AppCompatActivity {
         etLogin = findViewById(R.id.etLogin);
         etPassword = findViewById(R.id.etPassword);
 
+        users = new Utils().getUsers();
+
         btnLogin.setOnClickListener(view -> {
-            if (checkLogin(etLogin.getText().toString()) && checkPassword(etPassword.getText().toString())) {
+            if (checkLogin(etLogin.getText().toString(),etPassword.getText().toString())) {
                 startActivity();
             } else {
                 Toast.makeText(this,"Логин или пароль не правильный", Toast.LENGTH_SHORT).show();
@@ -38,14 +46,10 @@ public class MainActivity extends AppCompatActivity {
         finish();
     }
 
-    private boolean checkLogin(String login) {
-        for (int i = 0; i < Utils.logins.length; i++) {
-            if (Utils.logins[i].equals(login)) return true;
+    private boolean checkLogin(String login, String password) {
+        for (UserModel userModel : users ){
+            if (userModel.getLogin().equals(login) && userModel.getPassword().equals(password)) return true;
         }
         return false;
-    }
-
-    private boolean checkPassword(String password) {
-        return password.equals(Utils.password);
     }
 }
--- app/src/main/java/com/samaaps/architecture/Utils.java
+++ app/src/main/java/com/samaaps/architecture/Utils.java
@@ -1,6 +1,25 @@
 package com.samaaps.architecture;
 
+import java.util.ArrayList;
+
 public class Utils {
-    public static String[] logins = { "login1", "login2"};
-    public static String password = "pass";
+    private ArrayList<UserModel> users;
+
+    public Utils() {
+        users = new ArrayList<>();
+        users.add(new UserModel("login1", "Password1"));
+        users.add(new UserModel("login2", "Passwoslkdhbvklhjsdb"));
+        users.add(new UserModel("login3", "dknfbvkljdfn jkvndf"));
+        users.add(new UserModel("login4", "asmnd fkndfa k"));
+        users.add(new UserModel("login5", "a;ksjecvkljaer nkjcv"));
+        users.add(new UserModel("login6", "jsdhb cj rs"));
+    }
+
+    public ArrayList<UserModel> getUsers() {
+        return users;
+    }
+
+    public void setUsers(ArrayList<UserModel> users) {
+        this.users = users;
+    }
 }
